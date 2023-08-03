a113fb881778b835b1734f02aec332ca7139f3aa
Om Kokate <om.kokate2902@gmail.com>
2023-08-03 23:21:14 +0530

    react files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import WeatherForm from './components/WeatherForm';
+import WeatherInfo from './components/WeatherInfo';
+import axios from 'axios';
+import './App.css'; // Import your custom CSS file
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleWeatherData = (location) => {
+    const formData = new URLSearchParams();
+    formData.append('location', location);
+
+    // Set the proper headers for x-www-form-urlencoded data
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+
+    // Make the API request to the Django backend to get weather data
+    axios.post('http://localhost:8000/api/weather/', formData, config)
+      .then((response) => {
+        // Handle the response data and update the state
+        setWeatherData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-title">Weather App</h1>
+      <WeatherForm onWeatherData={handleWeatherData} />
+      {weatherData ? (
+        <WeatherInfo weatherData={weatherData} />
+      ) : (
+        <p className="loading-text">Enter a location to get the weather information.</p>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +12,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// Function to get CSRF token from Django's cookies
+function getCSRFToken() {
+  const cookieValue = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('csrftoken='))
+    .split('=')[1];
+  return cookieValue;
+}
+
+// Set the default X-CSRFToken header for all Axios requests
+axios.defaults.headers.common['X-CSRFToken'] = getCSRFToken();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
